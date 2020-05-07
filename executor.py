3b73263dc66fed53378638cb0df6ef153cfa0246
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2020-05-07 13:01:42 +0200

    refactor

--- executor.py
+++ executor.py
@@ -25,15 +25,39 @@ parser.add_argument('--nb', type=str, help='notebook path')
 parser.add_argument('--name', type=str, help='notebook name')
 args = parser.parse_args()
 
-def nb_exec(path):
-    dirpath = os.path.dirname(path)
+def nb_exec(fname, name):
+    logger.info(f'Ensuring {data_dir}')
+    ensure_dir(data_dir)
+    
+    nb_basepath = os.path.join(data_dir, name)
+    logger.info(f'Ensuring {nb_basepath}')
+    ensure_dir(nb_basepath)
 
-    with open(path) as f:
+    dirs = os.listdir(nb_basepath)
+    dirs = list(filter(lambda d: os.path.isdir(os.path.join(nb_basepath, d)), dirs))
+    dirs = sorted(list(map(lambda d: int(d), dirs)))
+
+    ver = "1"
+    if len(dirs) > 0:
+        logger.info('Last version')
+        ver = str(dirs[-1] + 1)
+    logger.info(f'Current version: {ver}')
+
+    dirpath = os.path.join(nb_basepath, ver)
+    execpath = os.path.join(dirpath, 'src')
+    src_nb = os.path.join(execpath, os.path.basename(fname))
+    ensure_dir(dirpath)
+    ensure_dir(execpath)
+    
+    logger.info(f'Copying {fname} to {src_nb}')
+    shutil.copy2(fname, src_nb)
+
+    with open(src_nb) as f:
         nb = nbformat.read(f, as_version=4)
 
     ep = ExecutePreprocessor(timeout=-1, kernel_name='python3')
     try:
-        ep.preprocess(nb, {'metadata': {'path': dirpath}})
+        ep.preprocess(nb, {'metadata': {'path': execpath}})
     except CellExecutionError as e:
         print(e)
         with open(os.path.join(dirpath, 'error.log'), 'w') as f:
@@ -51,29 +75,9 @@ def nb_exec(path):
         f.write(body)
 
 if __name__ == '__main__':
-    logger.info(f'Ensuring {data_dir}')
-    ensure_dir(data_dir)
-    nb_basepath = os.path.join(data_dir, args.name)
-    logger.info(f'Ensuring {nb_basepath}')
-    ensure_dir(nb_basepath)
-
-    dirs = os.listdir(nb_basepath)
-    dirs = list(filter(lambda d: os.path.isdir(os.path.join(nb_basepath, d)), dirs))
-    dirs = sorted(list(map(lambda d: int(d), dirs)))
-
-    ver = "1"
-    if len(dirs) > 0:
-        logger.info('Last version')
-        ver = str(dirs[-1] + 1)
-    logger.info(f'Current version: {ver}')
-
     fname = args.nb
-    destpath = os.path.join(nb_basepath, ver, os.path.basename(fname))
-    ensure_dir(os.path.join(nb_basepath, ver))
+    name = args.name
 
-    logger.info(f'Copying {fname} to {destpath}')
-    shutil.copy2(fname, destpath)
-
-    nb_exec(destpath)
+    nb_exec(fname, name)
 
     
